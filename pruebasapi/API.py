fa4a70594f4c75c063bcc1ae90d6ee733ea8722e
PabloMuralles <pablomuralles28@gmail.com>
2021-04-20 15:17:10 -0600

    pruebas

---
+++ pruebasapi/API.py
@@ -0,0 +1,53 @@
+from flask import Flask, jsonify, request
+import requests
+import mysql.connector
+from datetime import datetime
+from datetime import date
+
+db = mysql.connector.connect(
+    host='localhost',
+    user='pablo',
+    passwd='admin',
+    database="pruebas"
+    ) 
+
+mycursor = db.cursor()
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if(request.method == 'POST'):
+        tempJson = request.get_json()
+        print(tempJson) 
+        if (tempJson['20'] == '1'):
+            now = datetime.now().strftime("%H:%M:%S") 
+            dateD = str(date.today())
+            print(dateD) 
+            mycursor.execute("insert into data values (default,'{}','{}','on')".format(dateD,now))
+            db.commit()
+            return jsonify({'21':'True'}), 201
+        else:
+            now = datetime.now().strftime("%H:%M:%S")
+            dateD = str(date.today())
+            print(dateD)
+            mycursor.execute("insert into data values (default,'{}','{}','on')".format(dateD,now))
+            db.commit()
+            return jsonify({'21':'False'}), 201
+    else:
+        return jsonify({'about':'Hola Pablo'})
+
+
+@app.route('/showtable', methods=['GET'])
+
+def ShowTable():
+    mycursor.execute("select * from data;")
+    data = mycursor.fetchall()
+    return render_template('template.html', data=data)
+
+
+
+
+
+if (__name__ == '__main__'):
+    app.run(host='0.0.0.0',port=8080)
