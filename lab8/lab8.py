97fc29ada63d2df00a9deb16d4f7e7943dd2c40a
PabloMuralles <pablomuralles28@gmail.com>
2021-04-21 12:33:39 -0600

    apis de los labs

---
+++ lab8/lab8.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify, request, render_template
+import requests
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if(request.method == 'POST'):
+        tempJson = request.get_json()
+        print(tempJson) 
+        if (tempJson['20'] == '1'):
+            num = '11110011'
+            return jsonify({'num':('{}'.format(num))}), 201
+        else:
+            return jsonify({'Respuesta':'No hacer nada'})
+    else:
+        return jsonify({'about':'Hola Pablo'})
+
+
+
+if (__name__ == '__main__'):
+    app.run(host='0.0.0.0',port=8080)
