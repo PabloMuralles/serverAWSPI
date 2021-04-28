b9120294d8876da562447e1b609e3c1c455144d6
PabloMuralles <pablomuralles28@gmail.com>
2021-04-27 21:22:24 -0600

    repaso terminado ;

--- repasolab10/servicio.py
+++ repasolab10/servicio.py
@@ -1,43 +1,55 @@
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, redirect, url_for, jsonify, request, render_template
 import requests
 import mysql.connector
 from datetime import datetime
 from datetime import date
-#from json2html import * 
 
-'''db = mysql.connector.connect(
-    host='localhost',
-    user='pablo',
-    passwd='admin',
-    database="pruebas"
-    )'''
 
-#mycursor = db.cursor()
+data = {}
+open = False
+saveInfo = False
 
 app = Flask(__name__)
 
+#endpoint to communicate the rasberry with the web service
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if(request.method == 'POST'):
-        if request.form.get("submit_a"):
-			#status = request.page.get("onoff")
-            #print(status)
+        tempJson = request.get_json()
+        print(tempJson) 
+        global open, data, saveInfo
+        if (tempJson['20'] == '1'):
+            open = True
+            saveInfo = True
+            while True:
+                if (bool(data) != False ):
+                    break
+            temp = data
+            data = {}
+            saveInfo = False
+            return jsonify(temp)
+        elif (tempJson['20'] == '0'):
+            open = False
+            return ('',204)
     else:
-        return render_template("page.html")
+        return jsonify({'Hola':'Pablo'})
 
-'''@app.route('/showtable', methods=['GET'])
-def ShowTable():
-    mycursor.execute("select * from data;")
-    data = mycursor.fetchall()
-    return render_template('mariadb.html', data=data)
+#endpoint to communicate the the web page with web service
+@app.route('/p', methods=['GET','POST'])
+def page():
+    if(request.method == 'POST'):
+        global data 
+        tempData = request.form.get("finfo")
+        if(saveInfo == True):
+            data['data']= tempData
+        #return jsonify(data), 201
+        return ('',204)
+    else:
+        if (open != False):
+            return render_template("page.html"), 201
+        else:
+            return render_template("pagedown.html"), 201
 
-
-@app.route('/jsontable', methods=['GET'])
-def ShowTablePi():
-    mycursor.execute("select * from data;")
-    data = mycursor.fetchall()
-    return jsonify(data)
-'''
 
 if (__name__ == '__main__'):
     app.run(host='0.0.0.0',port=8080)
